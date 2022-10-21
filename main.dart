562a34b1cb3f718904f1a8bebb6b2ccda79857e8
Lucas Capalbo <38131754+RedSpeedyh@users.noreply.github.com>
2022-10-20 22:08:41 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:anime_list/view/tela_login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AnimeList',
+      home: TelaLogin(),
+    ),
+  );
+}
